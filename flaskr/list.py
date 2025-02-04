fae305d1919b6c0c112fc0d0bedb91e29b277330
SereqG <serekgrom@gmail.com>
2025-02-04 13:15:05 +0100

    To do list template

--- flaskr/list.py
+++ flaskr/list.py
@@ -5,4 +5,16 @@ bp = Blueprint("list", __name__, url_prefix="/")
 
 @bp.route("/")
 def list():
-    return render_template("list.html")
+    to_do_items = [
+        {"id": 1, "task_name": "Walk with the dog", "is_done": False, "time": "13:00"},
+        {
+            "id": 2,
+            "task_name": "Study math for 40 minutes",
+            "is_done": True,
+            "time": "15:00",
+        },
+        {"id": 3, "task_name": "Clean my room", "is_done": False, "time": "12:00"},
+        {"id": 4, "task_name": "Go to the gym", "is_done": False, "time": "18:00"},
+    ]
+
+    return render_template("list.html", items=to_do_items)
